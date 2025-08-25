ec50cdc592baef9068776e659797c4bf58ff54ba
JSeHoone <beebea545@gmail.com>
2025-08-25 17:38:36 +0900

    feat : 화면에 적용하면서 백엔드 기능 확인

---
+++ springbootPractice/src/main/java/com/sehoon/springbootPractice/controller/Homecontroller.java
@@ -0,0 +1,13 @@
+package com.sehoon.springbootPractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Homecontroller {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
